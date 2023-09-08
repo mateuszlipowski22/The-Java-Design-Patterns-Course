a16d61fdcf1105fb52d7d5e3f12054d0969fd55b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-09-08 23:32:56 +0200

    Design pattern - memento challenge

---
+++ src/main/java/com/udemy/section26/solution/Memento.java
@@ -0,0 +1,14 @@
+package com.udemy.section26.solution;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
